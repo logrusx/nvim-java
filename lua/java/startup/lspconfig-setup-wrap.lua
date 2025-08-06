5e6b3ceed7228225ad59c2c6dd4d07063c6060b8
logrusx <jorodrbr@yahoo.com>
2025-08-06 13:56:15 +0300

    cleaned something that was part of another feature

--- lua/java/startup/lspconfig-setup-wrap.lua
+++ lua/java/startup/lspconfig-setup-wrap.lua
@@ -38,7 +38,7 @@ function M.setup(config)
 
 		if config.spring_boot_tools.enable then
 			require('spring_boot').setup({
-				ls_path = vim.fn.glob(mason_util.get_pkg_path('vscode-spring-boot-tools')
+				ls_path = vim.fn.glob(mason_util.get_pkg_path('spring-boot-tools')
 					.. '/extension/language-server/spring-boot-language-server-*.jar'),
 			})
 
